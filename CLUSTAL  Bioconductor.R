e4d7b93cce8f2977925638929f73f8221557c481
Andrew Cinquini Lamothe <Alepidoptera7@gmail.com>
2023-10-26 14:50:57 -0700

    Add files via upload
    Problems with pdfLaTeX.

--- CLUSTAL  Bioconductor.R
+++ CLUSTAL  Bioconductor.R
@@ -12,41 +12,36 @@ library(msa)
 library(Biostrings)
 
 #check that requsite files extis
-file.exists("C:/Users/Quin The Conquoror!/PycharmProjects/Tuatara_mTs/SRR8351024.fastq.gz")
+#for m1 use 30, for m2 use 29
+file.exists("C:/Users/Quin The Conquoror!/Desktop/MN864229.fa")
 file.exists("C:/Users/Quin The Conquoror!/Desktop/M2_seqs.fa")
 
 #access the file paths
 
-ref_path <- file.path("C:/Users/Quin The Conquoror!/Desktop/M1_seqs.fa")#"C:/Users/Quin The Conquoror!/PycharmProjects/Tuatara_mTs/SRR8351024.fastq.gz")
+ref_path <- file.path("C:/Users/Quin The Conquoror!/Desktop/MN864229.fa")#"C:/Users/Quin The Conquoror!/PycharmProjects/Tuatara_mTs/SRR8351024.fastq.gz")
 ref_path
 
 fa_path <- file.path("C:/Users/Quin The Conquoror!/Desktop/M2_seqs.fa")
 fa_path
 
 #develop DNAStringSets for the ref and seqs
-seqs <- readDNAStringSet(fa_path)
+seqs <- readDNAStringSet(fa_path, format="fasta")
 seqs
 
-ref <- readDNAStringSet(ref_path)
+ref <- readDNAStringSet(ref_path, format="fasta")
 ref
 
 #now, to unpack the dnastringset one read at a time, developing a new fasta file. 
-
 for(i in 1:length((seqs))) names(seqs) <- paste0("M2_seqs_", seq(i))
 
 #write a fasta file for each seq, paired with the reference genome for CLUSTAL
-
-for(s in names(seqs)) writeXStringSet(seqs[s], paste0(s,".fa"))
-
+for(s in names(seqs)) writeXStringSet(seqs[s], paste0(s, ".fa"))
 
 #performing CLUSTAL on each seq against the reference genome.
-
-alignment <- msa(names(seqs))
-
+for(s in names(seqs)){
+alignment <- msa(seqs[s], ref, "ClustalOmega")
 alignment 
-
-print(alignment, show="complete")
-
-msaPrettyPrint(alignment, output="pdf", showNames = "True")
 }
+print(alignment, show="complete")
+msaPrettyPrint(alignment, output="tex", showNames = "none", verbose=FALSE, askForOverwrite = FALSE)
 
