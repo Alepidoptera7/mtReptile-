fc0b7a4282e46d86484d9b86279789130a3a1af1
Andrew Cinquini Lamothe <Alepidoptera7@gmail.com>
2023-10-30 13:31:24 -0700

    Add files via upload
    multi-read fasta file is used to generate fasta files with one read and the reference genome per file. clustal omega command line is used to generate command line prompts for windows powershell, to ultimately preform the alignments.

--- clustal_per_seq.py
+++ clustal_per_seq.py
@@ -1,55 +1,55 @@
+import Bio
 from Bio import AlignIO
-from Bio.Align.Applications import ClustalwCommandline
+from Bio import SeqIO
+from Bio.Align.Applications import ClustalOmegaCommandline
+from Bio import pairwise2
 
 class clustal_per_seq:
 
     def __init__(self):
         self.read_dict = {}
+        self.file_dict = {}
 
     def fasta_splitter(self):
         """Splits fasta file into one fasta file for each read sequence, containing only that read sequence and the reference genome."""
 
-        read_list = []
+        read_list, reference_list = [], []
         with open("M2_seqs.fa") as fasta:
             for line in fasta:
-                read_list = line.split("\n")
+                read_list.append(line)
 
-        print(len(read_list))
+        with open("MN864229.fa") as reference_fasta:
+            for line in reference_fasta:
+                reference_list.append(line)
+                #print(len(line))
+        print(len(read_list), len(reference_list))
 
         for i in range(len(read_list)):
-
             if ">" in read_list[i]:
-
                 self.read_dict[read_list[i]] = read_list[i+1]
 
-                new_file_name = read_list[i].replace(">", "").replace("@", "") + ".fa"
-                print(new_file_name)
-                new_file = open(new_file_name, "w")
-                new_file.write(read_list[i])
-                new_file.write(read_list[i+1])
+        for key in self.read_dict.keys():
+            new_file_name = key[2:-14] +".fa"
+            new_file = open(new_file_name, "w")
+            new_file.write(key)
+            new_file.write(self.read_dict[key])
+            new_file.write("\n")
 
-        #infile = "M2_seqs.fasta"
+            for line in reference_list:
+                new_file.write(line)
 
+            self.biopython_clustalw(new_file_name)
 
-        #outfile = "clustalw"+ seq_header + ".aln"
+    def biopython_clustalw(self, infile):
+        clustalOmega_exe = r"C:/Users/Quin The Conquoror!/Desktop/clustal-omega-1.2.2-win64/clustalo"
+        outfile = infile + "_aligned"
+        cline = ClustalOmegaCommandline(clustalOmega_exe, infile=infile, verbose=True, outfile=outfile, outfmt="fasta")
+        print("./"+str(cline))
 
-
-
-        #self.aligner(infile, outfile)
-
-    def aligner(self, infile, outfile):
-        cline = ClustalwCommandline("clustalw2", infile=infile, outfile=outfile)
-        print(cline)
-
-        format = "clustal"
-        align = AlignIO.read(outfile, format)
-
-        print(align)
 
     def driver(self):
         #self.fasta_reader()
         self.fasta_splitter()
-        print("OK")
 
 def main():
     class_access = clustal_per_seq()
